1cd6b52eaa0e998594f8c32e0a97d0c998cfe65a
DheenaBharath20 <bharathkuppu@gmail.com>
2021-10-02 09:22:49 +0530

    Thumbnail Generator - Redis Cache works

--- imageparotter/imageparotter/settings.py
+++ imageparotter/imageparotter/settings.py
@@ -121,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'media'))
+IMAGEDIR = os.path.join(MEDIA_ROOT, 'images')
+
+if not os.path.exists(MEDIA_ROOT) or not os.path.exists(IMAGEDIR):
+    os.makedirs(IMAGEDIR)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
